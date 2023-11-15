ed75fc9251e142d8c0bb6fcf6eee4018d423c560
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-11-15 12:43:16 +0200

    JS

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-// Wait for the page to load
-document.addEventListener('DOMContentLoaded', function() {
-  // Get a reference to the button element
-  var downloadBtn = document.getElementById('downloadBtn');
-  
-  // Add a click event listener to the button
-  downloadBtn.addEventListener('click', function() {
-    // Location of the PDF file
-    var pdfUrl = 'ymyResume.pdg';
-    
-    // Trigger the download
-    window.open(pdfUrl, '_blank');
-  });
-});
+//COMMING SOON!!
