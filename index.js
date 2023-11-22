52d05f6ec9e2b4378af5349a2b786b01fc991cdb
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-11-22 18:11:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-//COMMING SOON!!
+// function scrollToDiv(divId) {
+//     console.log("Function is working");
+//     var div = document.getElementById(divId);
+//     if (div) {
+//       div.scrollIntoView({ behavior: "smooth" });
+//     }
+// }
+
+
+const navbar = document.getElementById("header-hidden");
+let prevScrollPos = window.scrollY;
+
+window.addEventListener("scroll", function() {
+    let currentScrollPos = window.scrollY;
+
+    if (prevScrollPos > currentScrollPos) {
+        navbar.style.transform = "translateY(0)";
+    } else {
+        navbar.style.transform = "translateY(-100%)";
+    }
+
+    prevScrollPos = currentScrollPos;
+});
