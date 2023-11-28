d4f5036a19b94dcd4f60fe3e91197221ebe5c405
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-11-28 11:15:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,19 @@ window.addEventListener("scroll", function() {
 
     prevScrollPos = currentScrollPos;
 });
+
+
+// Wait for the page to load
+document.addEventListener('DOMContentLoaded', function() {
+  // Get a reference to the button element
+  var downloadBtn = document.getElementById('downloadBtn');
+  
+  // Add a click event listener to the button
+  downloadBtn.addEventListener('click', function() {
+    // Location of the PDF file
+    var pdfUrl = 'myResume.pdf';
+    
+    // Trigger the download
+    window.open(pdfUrl, '_blank');
+  });
+});
