65d717fdbdafcb83d1593c63f9b4f406ed49761a
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2023-12-06 23:13:19 +0200

    Update index.js

--- my-simple-website/index.js
+++ my-simple-website/index.js
@@ -31,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add a click event listener to the button
   downloadBtn.addEventListener('click', function() {
     // Location of the PDF file
-    var pdfUrl = 'MyResume_CV(1)(1).pdf';
+    var pdfUrl = 'Mycv.pdf';
     
     // Trigger the download
     window.open(pdfUrl, '_blank');
