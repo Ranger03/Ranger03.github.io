5d8e79b00a76f469e38a2a2c8a79f30e596d3078
Ranger03 <89092453+Ranger03@users.noreply.github.com>
2024-02-08 15:03:40 +0200

    updated-code

--- personal website/index.js
+++ personal website/index.js
@@ -1,40 +1,32 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let downloadBtn = document.getElementById("downloadBtn");
 
-$(document).ready(function() {
-  var lastScrollTop = 0;
-  var delta = 5;
-  var navbarHeight = $('#header-hidden').outerHeight();
 
-  $(window).scroll(function() {
-      var st = $(this).scrollTop();
-      
-      if (Math.abs(lastScrollTop - st) <= delta)
-          return;
+  downloadBtn.addEventListener("click", function () {
+    let pdfUrl = "Mycv.pdf";
 
-      if (st > lastScrollTop && st > navbarHeight){
-        
-          $('#header-hidden').removeClass('nav-down').addClass('nav-up');
-      } else {
-          
-          if(st + $(window).height() < $(document).height()) {
-              $('#header-hidden').removeClass('nav-up').addClass('nav-down');
-          }
-      }
-      
-      lastScrollTop = st;
+    window.open(pdfUrl, "_blank");
   });
 });
 
+$(document).ready(function () {
+  let lastScrollTop = 0;
+  let delta = 5;
+  let navbarHeight = $("#header-hidden").outerHeight();
 
-document.addEventListener('DOMContentLoaded', function() {
+  $(window).scroll(function () {
+    let st = $(this).scrollTop();
 
-  var downloadBtn = document.getElementById('downloadBtn');
-  
+    if (Math.abs(lastScrollTop - st) <= delta) return;
 
-  downloadBtn.addEventListener('click', function() {
- 
-    var pdfUrl = 'Mycv.pdf';
-    
- 
-    window.open(pdfUrl, '_blank');
+    if (st > lastScrollTop && st > navbarHeight) {
+      $("#header-hidden").removeClass("nav-down").addClass("nav-up");
+    } else {
+      if (st + $(window).height() < $(document).height()) {
+        $("#header-hidden").removeClass("nav-up").addClass("nav-down");
+      }
+    }
+
+    lastScrollTop = st;
   });
 });
